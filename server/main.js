7e33a7ce6a6190bcef24fee1af1f972922c5bfdc
sidoru <sidoru@gmail.com>
2020-02-14 16:33:02 +0900

    basic認証試す

--- server/main.js
+++ server/main.js
@@ -1,5 +1,4 @@
 import { Meteor } from 'meteor/meteor';
-import Links from '/imports/api/links';
 import Positions from '../imports/api/positions';
 
 function insertLink(title, url) {
@@ -7,28 +6,5 @@ function insertLink(title, url) {
 }
 
 Meteor.startup(() => {
-  console.log("データ初期化");
   Positions.remove({});
-  // If the Links collection is empty, add some data.
-  if (Links.find().count() === 0) {
-    insertLink(
-      'Do the Tutorial',
-      'https://www.meteor.com/tutorials/react/creating-an-app'
-    );
-
-    insertLink(
-      'Follow the Guide',
-      'http://guide.meteor.com'
-    );
-
-    insertLink(
-      'Read the Docs',
-      'https://docs.meteor.com'
-    );
-
-    insertLink(
-      'Discussions',
-      'https://forums.meteor.com'
-    );
-  }
 });
