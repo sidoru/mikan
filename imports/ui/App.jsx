77ad9933ce2a378e7c3d409681a39c0c7be26f05
sidoru <sidoru@gmail.com>
2020-02-19 18:25:02 +0900

    ログイン付けてみたけど遷移おかしい

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,54 +1,30 @@
 import React, { Component } from 'react';
-import Position from './Position.jsx';
+import { BrowserRouter as Router, Route, Link, Switch, withRouter } from 'react-router-dom';
+
+import List from '@material-ui/core/List';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+
+import Main from './Main.jsx';
 import ScheduleList from './ScheduleList.jsx';
 import Hello from './Hello.jsx';
 import ListItemLink from './ListItemLink.jsx';
-import { BrowserRouter as Router, Route, Link, Switch, } from 'react-router-dom';
-
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-
-import Divider from '@material-ui/core/Divider';
-import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
+import Login from './Login.jsx';
+import Signup from './Signup.jsx';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      selectedIndex: 0,
-    };
   }
+
 
   render() {
     return (
       <Router>
-        <div className="container">
-          <div className="menu-container">
-          <List component="nav">
-            <ListItemLink to="/" primary="ホーム" icon={<InboxIcon />} onClick={e => this.handleMenuItemClick(0)} selected={this.state.selectedIndex === 0} />
-            <ListItemLink to="/hello" primary="hello" icon={<DraftsIcon />} onClick={e => this.handleMenuItemClick(1)} selected={this.state.selectedIndex === 1} />
-          </List>
-          </div>
-
-          <div className="content">
-            <Switch>
-              <Route exact path="/" component={ScheduleList} />
-              <Route exact path="/schedules/:scheduleId/position" component={Position} />
-              <Route exact path="/hello" component={Hello} />
-              <Route exact component={ScheduleList} />
-            </Switch>
-          </div>
-        </div>
+        <Main />
       </Router>
     );
   }
-
-  handleMenuItemClick = index => {
-    this.setState({ selectedIndex: index })
-  };
 }
 
 export default App;
--- server/main.js
+++ server/main.js
@@ -1,8 +1,23 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base'
+
 import { Positions } from '../imports/api/collections';
 import '../imports/api/methods';
 import '../imports/api/publishes';
 
 Meteor.startup(() => {
+  if(Meteor.users.find().count() == 0){
+    let username = process.env.AUTH_USERNAME;
+    let password = process.env.AUTH_PASSWORD;
+    if (username === undefined) {
+      username = "user";
+      password = "pass";
+    }
+  
+    const id = Accounts.createUser({username, password});
+    console.log("id = " + id);
+  }
+
+  //Meteor.users.remove({});  
   //Positions.remove({});
 });
